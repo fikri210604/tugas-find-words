5e4ebc81bb20add3f59b5f91432eeebd7d08d1d3
Ahmad Fikri Hanif <144132158+fikri210604@users.noreply.github.com>
2024-04-28 13:01:33 +0700

    menambahkan Class cariKata ~ Fikri

--- findword.cpp
+++ findword.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+class cariKata{
+    std::vector<std::vector<char>> text;
+    std::vector<char> word,drow;
+    int panjangKata;
+    public:
+    cariKata(std::vector<std::vector<char>> text, std::vector<char> word) {
+        this->text = text;
+        this->word = word;
+        panjangKata = word.size();
+    }
+};
+
 int main(){
     std::ifstream inputFile("kata.txt");
     if (!inputFile.is_open()) {
@@ -14,8 +26,15 @@ int main(){
         for(char &c :kata)if(c!=' '){katakata.back().push_back(toupper(c));std::cout<<(char)toupper(c);}
         std::cout<<'\n';
     }
-
     inputFile.close();
-
+    std::cout<<"#input :\n";
+int n;
+std::cin>>n;  
+for(int i = 0; i < n; i++){
+    std::vector<char> words;
+    std::cin>>kata;
+    for(char &c : kata)words.push_back(toupper(c));
+    cariKata obj1(katakata,words);
+}
     return 0;
 }
